bc62c73dfe34ef44ba4f050210362c2cb3d74c46
Robin <robin007bond@gmail.com>
2015-11-19 10:38:58 +0100

    Add Hello World example

--- Jena-PoC/src/Test.java
+++ Jena-PoC/src/Test.java
@@ -1,7 +1,23 @@
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.log4j.BasicConfigurator;
+
+
 
 public class Test {
 	
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
 		System.out.println("Test");
+		
+		Model m = ModelFactory.createDefaultModel();
+		String NS = "http://example.com/test/";
+		Resource r = m.createResource ( NS + "r");
+		Property p = m.createProperty(NS + "p");
+		r.addProperty ( p, "Hello world", XSDDatatype.XSDstring);
+		m.write(System.out, "Turtle");
 	}
 }
